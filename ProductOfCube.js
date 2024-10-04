71f50b3be3ccc9c3072739599512dca358a62696
Thuvara <166202354+THuvarekeshY@users.noreply.github.com>
2024-10-04 10:24:02 +0530

    Add files via upload

---
+++ ProductOfCube.js
@@ -0,0 +1,18 @@
+let m=prompt("Enter value of m:")
+let n=prompt("Enter value of n:")
+let sum=0
+if(m>n){
+    document.write(0)
+    console.log(0);
+    
+}
+else{
+for(let i=m;i<=n;i++){
+    sum+=(i*i*i)
+}
+document.write(sum)
+console.log(sum);
+
+}
+
+
